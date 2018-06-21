afb6600a3f7758aaed13de1e24ac971e6808e853
Taylor Combs <t.combs140@gmail.com>
2018-06-21 17:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
   if (string.toUpperCase()===string)
   return "YES INDEED!"
-  if (string=="I love you, Grandma")
+  else
   return "I love you, too."
 }
