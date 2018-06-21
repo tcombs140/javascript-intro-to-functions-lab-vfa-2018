e4589d241dc6853859f941a460a4e51692618a93
Taylor Combs <t.combs140@gmail.com>
2018-06-21 17:40:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if string.toLowerCase()===string
     return "I can't hear you!"
+  if string.toUpperCase()===string
+    return ""
 }
